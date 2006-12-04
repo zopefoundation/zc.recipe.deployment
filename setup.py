8a929d3cccd0cab2cdb95a7923484f469940fcbe
Amos Latteier <amos@latteier.com>
2006-12-04 21:08:56 +0000

    Added the minimum necessary to build an egg. This package still needs lots of attention such as docs and tests.

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 name = 'zc.recipe.deployment'
 setup(
-    name=name,
-    entry_points='[zc.buildout]\ndefault=%s:Recipe' % name,
+    name = name,
+    entry_points = '[zc.buildout]\ndefault=%s:Recipe' % name,
     package_dir = {'': 'src'},
+    packages = find_packages('src'),
+    namespace_packages = ['zc', 'zc.recipe']
     )
 
