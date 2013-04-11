216684549140e94dc2afa7de1a52279be4be3afa
Fred Drake <fred@fdrake.net>
2013-04-11 01:57:11 +0000

    Preparing release 0.10.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
 
 setup(
     name = name,
-    version='0.10.2dev',
+    version='0.10.2',
     author = 'Jim Fulton',
     author_email = 'jim@zope.com',
     description = 'ZC Buildout recipe for Unix deployments',
